38a3e5c2e1c83e15697b80bf67a564d018caa949
Duftzi <107893343+Duftzi@users.noreply.github.com>
2022-07-14 16:10:10 -0700

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -25,8 +25,10 @@
 // xxxx.xxxx = byeSpeaker;
 
 (function(window) {
+	var byeSpeaker = {};
 	var speakWord = "Good Bye";
-	var byeSpeaker.speak = function (name) {
+	
+	byeSpeaker.speak = function (name) {
   		console.log(speakWord + " " + name);
 	}
 	
